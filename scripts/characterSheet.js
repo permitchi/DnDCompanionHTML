78451641b72c1568e786ad73f973d8ecbcfcd0ed
permitchi <mickavallery@gmail.com>
2025-11-02 23:06:09 +0700

    wireframe of UI in html

---
+++ scripts/characterSheet.js
@@ -0,0 +1,19 @@
+let str;
+let dex;
+let con;
+let int;
+let wis;
+let cha;
+
+fetch('data/data.json')
+    // .then(response => response.json())
+    // .then(data => {
+    //     attributeData = data;
+    // })
+    // .catch(error => console.error('Error loading data:', error));
+
+function countModifier(attribute) {
+    for (let i = 0; i <= data.attributes.length; i++) {
+        console.log("hi");
+    }
+}
